a97dbac4cbd0a14c28584ada08da66726712eca6
mnali825 <mna271@nyu.edu>
2017-07-18 19:48:57 +0000

    server side stuff

---
+++ getData.php
@@ -0,0 +1,53 @@
+<?php
+   $dbhost = 'localhost';
+   $dbuser = 'moniter';
+   $dbpass = 'password';
+   
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   $sql1 = 'SELECT * FROM tempTable ORDER BY timestamp DESC LIMIT 1';
+   $sql2 = 'SELECT * FROM phTable ORDER BY timestamp DESC LIMIT 1';
+   $sql3 = 'SELECT * FROM weightTable ORDER BY timestamp DESC LIMIT 1';
+   $sql4 = 'SELECT * FROM gasTable ORDER BY timestamp DESC LIMIT 1';
+
+   mysql_select_db('lostbytes');
+
+   $retval1 = mysql_query( $sql1, $conn );
+   $retval2= mysql_query( $sql2, $conn );
+   $retval3 = mysql_query( $sql3, $conn );
+   $retval4 = mysql_query( $sql4, $conn );
+     
+   if(! $retval1 ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   if(! $retval2 ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   if(! $retval3 ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   if(! $retval4 ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   
+   $row1 = mysql_fetch_array($retval1); 
+   $row2 = mysql_fetch_array($retval2);
+   $row3 = mysql_fetch_array($retval3);
+   $row4 = mysql_fetch_array($retval4);
+
+   $temp = $row1['temp'];
+   $ph = $row2['ph'];
+   $weight = $row3['weight'];
+   $gas = $row4['gas'];
+   
+   echo $temp . '|';
+   echo $ph . '|';
+   echo $weight . '|';
+   echo $gas . '|';
+   
+   mysql_close($conn);
+?>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <nav class="blue-grey darken-4">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo center">Lost Bytes</a>
+        </div>
+      </nav>
+      
+      <div class="container">
+        <div class="row">
+          <div class="card-panel col s12 m6 l6 center">
+            <h5>Temperature</h5>
+            <h1 id="temp-val"></h1>
+            <a style="margin-bottom:25px;" class="waves-effect waves-light btn">Full Data</a>
+          </div>
+          <div class="card-panel col s12 m6 l6 center">
+            <h5>pH Level</h5>
+            <h1 id="ph-val"></h1>
+            <a style="margin-bottom:25px;" class="waves-effect waves-light btn">Full Data</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="card-panel col s12 m6 l6 center">
+            <h5>Weight</h5>
+            <h1 id="weight-val"></h1>
+            <a style="margin-bottom:25px;" class="waves-effect waves-light btn">Full Data</a>
+          </div>
+          <div class="card-panel col s12 m6 l6 center">
+            <h5>Gas</h5>
+            <h1 id="gas-val"></h1>
+            <a style="margin-bottom:25px;" class="waves-effect waves-light btn">Full Data</a>
+          </div>
+        </div>
+      </div>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
+      <script>
+        document.addEventListener("DOMContentLoaded", init)
+        function init(event) {
+          var req = new XMLHttpRequest()
+          req.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+              var data = this.responseText;
+              var dataPieces = data.split('|')
+              document.getElementById('temp-val').textContent = dataPieces[0]
+              document.getElementById('ph-val').textContent = dataPieces[1]
+              document.getElementById('weight-val').textContent = dataPieces[2]
+              document.getElementById('gas-val').textContent = dataPieces[3] || 'No'
+            }
+          }  
+          req.open('GET', './getData.php', true)
+          req.send()        
+          setInterval(function() {
+            req.open('GET', './getData.php', true)
+            req.send()
+          }, 1500)
+
+        }
+      </script>
+    </body>
+  </html>
